9eea0b12e21bbc2749fd53cf8200a79bf82c01c1
adjustyourtone <anthonyrizzotto@gmail.com>
2021-01-24 14:36:53 -0500

    Add About Me text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import '../stylesheets/About.css';
-// import TonyImg from '../images/47800412.jpg';
 
 const AboutMe = () => {
   return (
     <div id="about">
       <div id="about-text" className="">
-        <h2>This is the about me component.</h2>
-        <h4>Fast, Reliable and Creative</h4>
+        <h2>
+          Hi! My name is Tony Rizzotto and I'm a{' '}
+          <i>Full Stack Web Developer</i> based in Boston, MA.
+        </h2>
+        <h3>Fast, Reliable and Creative</h3>
         <p>
           As a career creative, I've applied my unique skills towards building
-          real-world applications. Always an educator, I've been teaching young
-          children the art of web programming for 2 years.
+          real-world applications. Always an educator, I've been teaching
+          children music and programming at my online school fretscapes.com for
+          over 7 years.
         </p>
       </div>
     </div>
